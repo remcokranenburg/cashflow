c248dffe26152aa699e9b4a6dcbd62551ccc7941
Remco Kranenburg <remco@burgsoft.nl>
2021-02-24 12:49:22 +0100

    Generalize datatable template to cover balance

--- views.py
+++ views.py
@@ -16,11 +16,14 @@ class Balance:
         year_month = func.date(IngTransaction.date, "start of month")
         result = (
             session.query(
-                year_month,
+                year_month
+                    .label("month"),
                 func.sum(IngTransaction.amount)
-                    .over(partition_by=year_month),
+                    .over(partition_by=year_month)
+                    .label("amount"),
                 func.last_value(IngTransaction.balance_after_mutation)
                     .over(partition_by=year_month)
+                    .label("balance")
             )
             .distinct(IngTransaction.date)
             .order_by(IngTransaction.date)
@@ -32,8 +35,11 @@ class Balance:
     def as_view(self, request):
 
         data = self.retrieve_data()
+        columns = data[0].keys() if len(data) > 0 else []
 
-        return render_template("balance.html", page_title=self.page_title,
+        return render_template("datatable.html",
+                               page_title=self.page_title,
+                               columns=columns,
                                table=data)
 
 
@@ -74,6 +80,7 @@ class IngChecking:
         data = self.retrieve_data()
         return render_template("datatable.html",
                                page_title=self.page_title,
+                               upload_form_visible=True,
                                upload_message=self.upload_message,
                                columns=inspect(IngTransaction).columns.keys(),
                                table=data)
