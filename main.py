e576d68f6eb9bc8478080711591b8399cb11ddb9
Remco Kranenburg <remco@burgsoft.nl>
2022-12-02 15:02:34 +0100

    Gtk interface

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
+#!/usr/bin/env python3
+
+import sys
+
+import gi
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+from gi.repository import Gtk, Gio, GObject, GLib
+
+from views import Balance
+
+
+def on_activate(application: Gtk.Application):
+    window = Gtk.ApplicationWindow(application=application, title="Cashflow")
+    window.set_default_size(600, 250)
+
+    header = Gtk.HeaderBar()
+    window.set_titlebar(header)
+
+    balance_view = Balance().as_view()
+
+    window.set_child(balance_view)
+
+    window.present()
+
+
+def main(argv: list[str]):
+    application = Gtk.Application(application_id="com.remcokranenburg.Cashflow")
+    application.connect("activate", on_activate)
+    application.run(argv)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+
+"""
 from flask import Flask, render_template, request
 
 from views import Balance, IngChecking, Meesman
@@ -22,4 +59,4 @@ def ing_checking():
 @app.route("/meesman", methods=["GET", "POST"])
 def meesman():
     return Meesman().as_view(request)
-
+"""
--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 import csv
 
-from flask import render_template
+from gi.repository import Gtk, Gio, GObject, GLib
+
 from sqlalchemy import func, inspect
 from sqlalchemy.sql import select, text
 
@@ -8,8 +9,42 @@ from models import Base, IngTransaction, MeesmanBalance
 from settings import DBSession
 
 
+LIST_ITEM_UI = """\
+<?xml version="1.0" encoding="UTF-8"?>
+<interface>
+    <template class="GtkListItem">
+        <property name="child">
+            <object class="GtkLabel">
+                <property name="xalign">0</property>
+                <binding name="label">
+                    <lookup name="name" type="views+BalanceRecord">
+                        <lookup name="item">GtkListItem</lookup>
+                    </lookup>
+                </binding>
+            </object>
+        </property>
+    </template>
+</interface>
+"""
+
+
 def year_month(model):
     return func.date(model.date, "start of month")
+
+
+class BalanceRecord(GObject.GObject):
+    def __init__(self, name: str, age: float):
+        super().__init__()
+        self._name = name
+        self._age = age
+
+    @GObject.Property(type=str)
+    def name(self):
+        return self._name
+
+    @GObject.Property(type=float)
+    def age(self):
+        return self._age
 
 
 class Balance:
@@ -55,145 +90,20 @@ class Balance:
         session.close()
         return result
 
-    def as_view(self, request):
+    def as_view(self):
+        balance_list = Gio.ListStore.new(BalanceRecord)
+        balance_list.append(BalanceRecord("Jack", 34.8))
+        balance_list.append(BalanceRecord("Andrea", 37.0))
+        balance_list.append(BalanceRecord("Kim", 24.3))
 
         data = self.retrieve_data()
         columns = data[0].keys() if len(data) > 0 else []
 
-        return render_template("datatable.html",
-                               page_title=self.page_title,
-                               columns=columns,
-                               table=data)
+        selection = Gtk.SingleSelection.new(balance_list)
+        list_item_ui_bytes = GLib.Bytes.new(LIST_ITEM_UI.encode("utf-8"))
+        factory = Gtk.BuilderListItemFactory.new_from_bytes(None, list_item_ui_bytes)
 
-
-class Datatable:
-    model = None
-    page_title = "Table"
-    upload_message = None
-
-    def persist_data(self, table):
-        session = DBSession()
-
-        for i, row in enumerate(table):
-            if len(row) == 0:
-                continue
-
-            if i == 0:
-                print("skipping header")
-                continue
-
-            session.add(self.model.from_csv_line(row))
-
-        session.commit()
-        session.close()
-
-    def retrieve_data(self):
-        session = DBSession()
-        result = (session.query(self.model)
-                  .order_by(self.model.date)
-                  .all())
-        session.close()
-        return result
-
-    def as_view(self, request):
-        self.request = request
-
-        if request.method == "POST":
-            self.process_post()
-
-        data = self.retrieve_data()
-        return render_template("datatable.html",
-                               page_title=self.page_title,
-                               upload_form_visible=True,
-                               upload_message=self.upload_message,
-                               columns=inspect(self.model).columns.keys(),
-                               table=data)
-
-    def process_post(self):
-        upload_message = None
-
-        if "data.csv" in self.request.files:
-            file = self.request.files["data.csv"]
-            file_contents = file.read().decode("utf-8")
-            dialect = csv.Sniffer().sniff(file_contents)
-            table = csv.reader(file_contents.split("\n"), dialect)
-            self.persist_data(table)
-            self.upload_message = "CSV file imported"
-
-
-class IngChecking(Datatable):
-    model = IngTransaction
-    page_title = "ING Checking"
-
-
-class Meesman(Datatable):
-    model = MeesmanBalance
-    page_title = "Meesman"import csv
-
-from flask import render_template
-from sqlalchemy import func, inspect
-from sqlalchemy.sql import select, text
-
-from models import Base, IngTransaction, MeesmanBalance
-from settings import DBSession
-
-
-def year_month(model):
-    return func.date(model.date, "start of month")
-
-
-class Balance:
-    page_title = "Balance"
-    upload_message = None
-
-    def retrieve_data(self):
-        session = DBSession()
-
-        statement = text("""
-            with
-                ing_checking as (
-                    select distinct
-                        date(date, 'start of month') as month,
-                        last_value(balance_after_mutation)
-                            over (partition by date(date, 'start of month'))
-                            as balance
-                    from ingtransactions
-                    order by date
-                ),
-                meesman as (
-                    select distinct
-                        date(date, 'start of month') as month,
-                        last_value(value)
-                            over (partition by date(date, 'start of month'))
-                            as balance
-                    from meesmanbalances
-                    order by date
-                )
-            select
-                ing_checking.month,
-                ing_checking.balance as ing_checking,
-                meesman.balance as meesman,
-                (
-                    coalesce(ing_checking.balance, 0)
-                    + coalesce(meesman.balance, 0)
-                ) as net_worth
-            from ing_checking
-            left outer join meesman on ing_checking.month=meesman.month
-        """)
-
-        result = list(session.execute(statement))
-        session.close()
-        return result
-
-    def as_view(self, request):
-
-        data = self.retrieve_data()
-        columns = data[0].keys() if len(data) > 0 else []
-
-        return render_template("datatable.html",
-                               page_title=self.page_title,
-                               columns=columns,
-                               table=data)
+        return Gtk.ListView.new(selection, factory)
 
 
 class Datatable:
